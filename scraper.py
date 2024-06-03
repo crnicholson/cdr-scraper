26a14918d9dbdb918213b696f923f9fee5adf11d
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-03 17:37:11 -0400

    working full page gathering

--- scraper.py
+++ scraper.py
@@ -1,25 +1,33 @@
 import time
 from selenium import webdriver
 from bs4 import BeautifulSoup as soup
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-path = "chromedriver"
+path = "/Users/Charlie/Documents/Code/webScraper/chromedriver"
+
+# URL to be scraped
 url = "https://www.scienceopen.com/search"
 
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 
-driver = webdriver.Chrome(path, options=options)
-
-time.sleep(5)
+service = Service(path)
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.get(url)
+
+time.sleep(0.5)
 
 content = driver.page_source
 
 driver.quit()
 
-page = soup(content, 'html.parser')
+page = soup(content, "html.parser")
 
-print(page.prettify())
+page = page.prettify()
+
+f  = open("page.txt", "w")
+f.write(page)
+f.close()
