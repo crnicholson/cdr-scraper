37c24ab352c4c5cf3190661e2b03ffefc2607804
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-05 16:57:07 -0400

    Trying to download

--- scraper.py
+++ scraper.py
@@ -1,77 +1,115 @@
-# To-do
-# - Scrape the entire page
-# - Get the link again
-# - Download the paper
-# - Put the paper into a folder
-# - Automatically make a spreadsheet with the paper title, the paper link, the SJR quintile, and the paper itself
+# # To-do
+# # - Scrape the entire page
+# # - Get the link again
+# # - Download the paper
+# # - Put the paper into a folder
+# # - Automatically make a spreadsheet with the paper title, the paper link, the SJR quintile, and the paper itself
 
-import time
-import pandas as pd
+# import time
+# import pandas as pd
+# from selenium import webdriver
+# from bs4 import BeautifulSoup as soup
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
+
+# # Path to ChromeDriver installation - no relative paths
+# path = "/Users/Charlie/Documents/Code/webScraper/chromedriver"
+
+# # URL to be scraped
+# url = "https://www.scienceopen.com/search#('v'~4_'id'~''_'queryType'~1_'context'~null_'kind'~77_'order'~1_'orderLowestFirst'~false_'query'~'carbon%20capture'_'filters'~!('kind'~38_'not'~false_'offset'~2_'timeUnit'~7)*_'hideOthers'~false)"
+
+# df = pd.read_csv("scimagojr-2023.csv", sep=";", decimal=",")
+
+# options = Options()
+# options.add_argument("--headless")
+# options.add_argument("--disable-gpu")
+
+# service = Service(path)
+# driver = webdriver.Chrome(service=service, options=options)
+
+# driver.get(url)
+# time.sleep(0.5)
+# pageContent = driver.page_source
+
+# parsedPage = soup(pageContent, "html.parser")
+
+# allTitles = parsedPage.find_all(class_="so-article-list-item-title")
+
+# papers = {}
+
+# for titles in allTitles:
+#     foundLink = titles.find("a")
+#     if foundLink != None:
+#         paperUrl = foundLink["href"]
+#         paperTitle = foundLink.get_text(strip=True)
+#         papers[paperTitle] = paperUrl
+
+# keys = list(papers)
+
+# driver = webdriver.Chrome(service=service, options=options)
+
+# for key in keys:
+#     driver.get(papers[key])
+#     pageContent = driver.page_source
+#     parsedPage = soup(pageContent, "html.parser")
+#     issn = parsedPage.find(itemprop="issn")
+#     if issn != None:
+#         issn = issn.get_text(strip=True)
+#         issn = issn[0:4] + issn[5:9]
+#         newDf = df[df["Issn"].str.contains(issn)]
+#         sjr = newDf.iloc[0, 6]
+#         # print("Link: "+ papers[key] + " with an SJR quintile of: " + str(sjr))
+#         if sjr != "Q1":
+#             del papers[key]
+#     else:
+#         del papers[key]
+
+# url = "https://www.scienceopen.com/document?vid=af69a724-0f56-4e4a-aaa9-7bde2f866333"
+
+# vid = url[41:]
+
+# downloadUrl = "https://www.scienceopen.com/document?-1.ILinkListener-header-action~bar-download~dropdown-pdf~link-link&vid=" + vid
+
+# print(downloadUrl)
+
+# print(papers)
+
+# driver.quit()
+
 from selenium import webdriver
-from bs4 import BeautifulSoup as soup
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-# Path to ChromeDriver installation - no relative paths
+import time
+
+options = webdriver.ChromeOptions()
+
+prefs = {"download.default_directory": "/Users/Charlie/Documents/Code/webScraper/papers"}
+
+options.add_experimental_option("prefs", prefs)
+
 path = "/Users/Charlie/Documents/Code/webScraper/chromedriver"
 
-# URL to be scraped
-url = "https://www.scienceopen.com/search#('v'~4_'id'~''_'queryType'~1_'context'~null_'kind'~77_'order'~1_'orderLowestFirst'~false_'query'~'carbon%20capture'_'filters'~!('kind'~38_'not'~false_'offset'~2_'timeUnit'~7)*_'hideOthers'~false)"
-
-df = pd.read_csv("scimagojr-2023.csv", sep=";", decimal=",")
-
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-
 service = Service(path)
-driver = webdriver.Chrome(service=service, options=options)
 
-driver.get(url)
-time.sleep(0.5)
-pageContent = driver.page_source
+driver = webdriver.Chrome(service=service, chrome_options=options)
 
-parsedPage = soup(pageContent, "html.parser")
+try:
 
-allTitles = parsedPage.find_all(class_="so-article-list-item-title")
+    driver.get("https://www.browserstack.com/test-on-the-right-mobile-devices")
 
-papers = {}
+    downloadcsv = driver.find_element_by_css_selector(".icon-csv")
 
-for titles in allTitles:
-    foundLink = titles.find("a")
-    if foundLink != None:
-        paperUrl = foundLink["href"]
-        paperTitle = foundLink.get_text(strip=True)
-        papers[paperTitle] = paperUrl
+    gotit = driver.find_element_by_id("accept-cookie-notification")
 
-keys = list(papers)
+    gotit.click()
 
-driver = webdriver.Chrome(service=service, options=options)
+    downloadcsv.click()
 
-for key in keys:
-    driver.get(papers[key])
-    pageContent = driver.page_source
-    parsedPage = soup(pageContent, "html.parser")
-    issn = parsedPage.find(itemprop="issn")
-    if issn != None:
-        issn = issn.get_text(strip=True)
-        issn = issn[0:4] + issn[5:9]
-        newDf = df[df["Issn"].str.contains(issn)]
-        sjr = newDf.iloc[0, 6]
-        # print("Link: "+ papers[key] + " with an SJR quintile of: " + str(sjr))
-        if sjr != "Q1":
-            del papers[key]
-    else:
-        del papers[key]
-        
-url = "https://www.scienceopen.com/document?vid=af69a724-0f56-4e4a-aaa9-7bde2f866333"
+    time.sleep(5)
 
-vid = url[41:]
+    driver.close()
 
-downloadUrl = "https://www.scienceopen.com/document?-1.ILinkListener-header-action~bar-download~dropdown-pdf~link-link&vid=" + vid
+except:
 
-print(downloadUrl)
-
-print(papers)
-
-driver.quit()
+    print("Invalid URL")
