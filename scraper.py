77523a59e638e3ad9a7b522fc37b0ca57def8a8f
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-05 07:58:30 -0400

    Working version

--- scraper.py
+++ scraper.py
@@ -16,6 +16,8 @@ path = "/Users/Charlie/Documents/Code/webScraper/chromedriver"
 # URL to be scraped
 url = "https://www.scienceopen.com/search#('v'~4_'id'~''_'queryType'~1_'context'~null_'kind'~77_'order'~1_'orderLowestFirst'~false_'query'~'carbon%20capture'_'filters'~!('kind'~38_'not'~false_'offset'~2_'timeUnit'~7)*_'hideOthers'~false)"
 
+df = pd.read_csv("scimagojr-2023.csv", sep=";", decimal=",")
+
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
@@ -29,11 +31,7 @@ pageContent = driver.page_source
 
 parsedPage = soup(pageContent, "html.parser")
 
-print(parsedPage.prettify())
-
 allTitles = parsedPage.find_all(class_="so-article-list-item-title")
-
-print(allTitles)
 
 papers = {}
 
@@ -49,30 +47,20 @@ keys = list(papers)
 for key in keys:
     driver = webdriver.Chrome(service=service, options=options)
     driver.get(papers[key])
-    time.sleep(0.5)
+    # time.sleep(0.5)
     pageContent = driver.page_source
     parsedPage = soup(pageContent, "html.parser")
     issn = parsedPage.find(itemprop="issn")
     if issn != None:
         issn = issn.get_text(strip=True)
         issn = issn[0:4] + issn[5:9]
-        print(issn)
         papers[key] = issn
+        newDf = df[df["Issn"].str.contains(issn)]
+        sjr = newDf.iloc[0, 5]
+        print("ISSN: "+ issn + " with an SJR of: " + str(sjr))
     else:
         del papers[key]
 
-keys = list(papers)
 
-print(papers)
-
-df = pd.read_csv("scimagojr-2023.csv", sep=";", decimal=",")
-
-issn = "17594782"
-
-newDf = df[df["Issn"].str.contains(issn)]
-
-sjr = newDf.iloc[0, 5]
-
-print("SJR: " + str(sjr))
 
 driver.quit()
